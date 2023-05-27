ede3c65f04de5f0da6832116017dc187dbb4a52b
Raju Das <130568480+rajudasinfo@users.noreply.github.com>
2023-05-27 11:54:21 +0530

    Add files via upload

---
+++ access.js
@@ -0,0 +1,71 @@
+window.addEventListener('resize',()=>{
+
+  if(window.innerWidth <= 500){
+    document.body.remove();
+    alert("🙏 This game only youcan access  with PC or Tab with physical keyboard.");
+  }
+});
+
+const div = document.getElementById('con');
+let topi = 0;
+let lefti = 0;
+speed = 6;
+
+// let carch = document.getElementById('crach');
+// const when_run = ()=>{
+//    carch.src = "./audio/spaceship-cruising-ufo-7176.mp3";
+// };
+
+const knowKey = (e)=>{
+
+  switch(e.key){
+    case "ArrowUp":
+    topi = topi - speed;
+   // when_run();
+    break;    
+    case 'ArrowDown':
+    topi = topi + speed;
+   // when_run();
+    break;    
+    case 'ArrowRight':
+    lefti = lefti + speed;
+   // when_run();
+    break;    
+    case 'ArrowLeft':
+        lefti = lefti - speed;
+       // when_run();
+    break;    
+    default :
+    
+    console.log("press any of them : ArrowUp   ArrowDown ArrowLeft ArrowRight");
+  }
+ 
+if(topi < 0){
+    topi = 0;
+    when_crach();
+}else if(topi >=86){
+  topi = 86;
+  when_crach();
+}
+if(lefti < 0){
+    lefti = 0;
+    when_crach();
+}else if(lefti >= 92.9){
+    lefti = 92.7;
+    when_crach();
+}
+
+  div.style.cssText = `top:${topi}vh`;
+  div.style.cssText += `left :${lefti}vw`;
+};
+window.addEventListener('keydown',knowKey);
+
+// window.addEventListener('keyup',()=>{
+//   carch.src = "";
+// });
+
+//  top :86vh;
+//left: 92.8vw;
+
+
+
